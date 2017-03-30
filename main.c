38e3cd0404a64132c3f4902f81013fc76481e58a
RafaelLDSX <rafael_ldsx@hotmail.com>
2017-03-30 03:12:42 +0000

    Inserindo Tela de Fundo(intro.png)

--- main.c
+++ main.c
@@ -2,8 +2,14 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 int main(){
-	SDL_Window * window;
-	window = SDL_CreateWindow("Space Rage", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1920, 1080, SDL_WINDOW_FULLSCREEN_DESKTOP);
-	SDL_Delay(9900);
+	SDL_Window * janela;
+	SDL_Renderer * renderizador;
+	SDL_Texture * telaDeFundo;
+	janela = SDL_CreateWindow("Space Rage", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1920, 1080, SDL_WINDOW_FULLSCREEN_DESKTOP);
+	renderizador = SDL_CreateRenderer(janela, -1, SDL_RENDERER_ACCELERATED);
+	telaDeFundo = IMG_LoadTexture(renderizador, "intro.png");
+	SDL_RenderCopy(renderizador, telaDeFundo, NULL, NULL);
+	SDL_RenderPresent(renderizador);
+	SDL_Delay(1000);
 	return 0;
 }

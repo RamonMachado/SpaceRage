3992d4d875ef6d66f64ce62c1701a7cb7093d460
Rafael <rafael_ldsx@hotmail.com>
2017-03-30 21:53:45 +0000

    Nova resolução + Logo

--- main.c
+++ main.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+
 int main(){
 	SDL_Window * janela;
 	SDL_Renderer * renderizador;
 	SDL_Texture * telaDeFundo;
-	janela = SDL_CreateWindow("Space Rage", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1920, 1080, SDL_WINDOW_FULLSCREEN_DESKTOP);
+	SDL_Texture * logo;
+	SDL_Rect logoRect;
+	logoRect.x = 212;
+	logoRect.y = 134;
+	logoRect.w = 854;
+	logoRect.h = 450;
+	janela = SDL_CreateWindow("Space Rage", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_FULLSCREEN);
 	renderizador = SDL_CreateRenderer(janela, -1, SDL_RENDERER_ACCELERATED);
 	telaDeFundo = IMG_LoadTexture(renderizador, "intro.png");
+	logo = IMG_LoadTexture(renderizador, "sp.png");
 	SDL_RenderCopy(renderizador, telaDeFundo, NULL, NULL);
 	SDL_RenderPresent(renderizador);
-	SDL_Delay(5000);
+	SDL_Delay(1000);
+	SDL_RenderCopy(renderizador, logo, NULL, &logoRect);
+	SDL_RenderPresent(renderizador);
+	SDL_Delay(3000);
 	SDL_DestroyTexture(telaDeFundo);
 	SDL_DestroyRenderer(renderizador);
 	SDL_DestroyWindow(janela);
